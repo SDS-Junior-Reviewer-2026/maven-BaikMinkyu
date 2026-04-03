e89a3ca560170aa83a5fa0ec4b538b24e4ff1850
0713jake <0713jake@naver.com>
2026-04-03 16:25:07 +0900

    Added failing test

--- src/test/java/CalTest.java
+++ src/test/java/CalTest.java
@@ -3,11 +3,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalTest {
-
     @Test
     void basicUnitTest() {
         Cal cal = new Cal();
         int ret = cal.getSum(10, 20);
         assertEquals(30, ret);
+    }
+
+    @Test
+    void basicUnitTestFail() {
+        Cal cal = new Cal();
+        int ret = cal.getSum(10, 20);
+        assertEquals(1, ret);
     }
 }
